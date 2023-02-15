03afab2dd8d3607a89bac701e307e8925ed88d08
remcostoeten <remcostoeten@hotmail.com>
2023-02-15 18:59:47 +0100

    Add dope gradient effect to intro heading.

---
+++ components/layout/Intro.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+export default function Intro() {
+	return (
+		<>
+			<article className='container intro'>
+				<div className='intro__heading'>
+					<span>
+						turning ideas into
+						<br />
+					</span>
+					<span>
+						real life
+						<span className='intro__hero--heading-gradient'>
+							&nbsp;products
+							<br />
+						</span>
+						<span>is my calling.</span>
+						<br />
+					</span>
+					{/* <h1>
+					remco stoeten
+					<br />
+					front-end developer
+					<br />
+					apsiring to be more than a divjesschuiver.
+				</h1> */}
+				</div>
+			</article>
+		</>
+	);
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,7 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import { Inter } from '@next/font/google';
-import styles from '@/styles/Home.module.css';
-
-const inter = Inter({ subsets: ['latin'] });
+import Intro from '@/components/layout/Intro';
 
 export default function Home() {
 	return (
@@ -20,7 +18,9 @@ export default function Home() {
 				/>
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
-			<main></main>
+			<main className='homepage'>
+				<Intro />
+			</main>
 		</>
 	);
 }
