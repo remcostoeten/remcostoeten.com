551d7151247257de8061ff4adf609e0d07ddbca6
remcostoeten <remcostoeten@hotmail.com>
2023-02-15 22:34:11 +0100

    Add favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,7 +17,7 @@ export default function Home() {
 					name='viewport'
 					content='width=device-width, initial-scale=1'
 				/>
-				<link rel='icon' href='/favicon.ico' />
+				<link rel='icon' href='/favicon/favicon.ico' />
 			</Head>
 			<main className='homepage'>
 				<Intro />
