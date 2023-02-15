5848b1f6a92109f5598dbbf3609dc4507a2c3734
remcostoeten <remcostoeten@hotmail.com>
2023-02-15 22:35:20 +0100

    Add wihtespace

--- components/layout/UnderConstruction.tsx
+++ components/layout/UnderConstruction.tsx
@@ -4,14 +4,14 @@ export default function UnderConstruction() {
 	return (
 		<>
 			<div className='notice'>
-				Site under construction. Link to my github{' '}
+				Site under construction. Link to my github
 				<a
 					href='https://github.com/remcostoeten'
 					target='_blank'
 					rel='noreferrer'>
 					here
 				</a>
-				. And mail through
+				. And mail through&nbsp;
 				<a href='mailto:remcostoeten@hotmail'>here</a> or{' '}
 				<a href='https://wa.me/0636590707'>text/whatsapp</a>.
 			</div>
