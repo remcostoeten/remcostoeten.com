7c3ef306b10efc6e458329986ce81891b99778b6
remcostoeten <remcostoeten@hotmail.com>
2023-02-15 22:50:25 +0100

    Spacing.

--- components/layout/UnderConstruction.tsx
+++ components/layout/UnderConstruction.tsx
@@ -4,7 +4,7 @@ export default function UnderConstruction() {
 	return (
 		<>
 			<div className='notice'>
-				Site under construction. Link to my github
+				Site under construction. Link to my Github &nbsp;
 				<a
 					href='https://github.com/remcostoeten'
 					target='_blank'
---
+++ styleguide.md
@@ -0,0 +1,1 @@
+#https://www.adeolaadeoti.xyz/
