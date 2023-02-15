a36d6ca2efb23494b218d91e9f9a91c0cabb9b78
remcostoeten <remcostoeten@hotmail.com>
2023-02-15 23:19:26 +0100

    LLinks

--- components/Menu.js
+++ components/Menu.js
@@ -4,18 +4,17 @@ import styles from './HamburgerMenu/HamburgerMenu.module.scss';
 
 const Menu = ({ className }) => (
 	<div className={className}>
+		Temporary menu.
 		<ul className={styles.navigation__list}>
 			<li className={styles.navigation__item}>
-				<a href=''>ABC</a>{' '}
+				<a href='https://remcostoeten-multistepform.netlify.app/'>
+					Frontendmentor multistrepform in react
+				</a>{' '}
 			</li>
 			<li className={styles.navigation__item}>
-				<a href=''>DEF</a>
-			</li>
-			<li className={styles.navigation__item}>
-				<a href=''>GHI</a>
-			</li>
-			<li className={styles.navigation__item}>
-				<a href=''>JKL</a>
+				<a href='https://funny-elf-e20c8d.netlify.app//'>
+					Frontendmentor chat case in vanillaJS + HTML/SCSS
+				</a>{' '}
 			</li>
 		</ul>
 	</div>
