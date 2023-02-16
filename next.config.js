fb0717ed313e005fa2126f882e402b68d2e1bbdd
remcostoeten <remcostoeten@hotmail.com>
2023-02-16 14:12:23 +0100

    Add netlify auth and set tsx less strict.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+	reactStrictMode: false,
+	ignoreBuildErrors: false,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
