b1b45f140c1453adaa1a72cb998c0771fe70c005
remcostoeten <remcostoeten@hotmail.com>
2023-02-23 01:11:36 +0100

    wip

---
+++ components/Todos/todos.ts
@@ -0,0 +1,17 @@
+export const Data = [
+	{
+		id: '0',
+		task: 'Soliciteren',
+		isCompleted: 'false',
+	},
+	{
+		id: '1',
+		task: 'blabl',
+		isCompleted: 'true',
+	},
+	{
+		id: '2',
+		task: 'slapen',
+		isCompleted: 'false',
+	},
+];
---
+++ pages/api/chatDisplayData.js
@@ -0,0 +1,10 @@
+export const chatDisplayData = {
+	chat: [
+		{ message: ' This  is a message ' },
+		{ message: ' This also ' },
+		{ message: 'Wrote this js data my self' },
+		{
+			message: 'I have more data laying arround e.g whatsapp export',
+		},
+	],
+};
