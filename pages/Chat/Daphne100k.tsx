cab7bedcc2e537972200e0bdcc62d38e2e9c8bf7
remcostoeten <57683378+remcostoeten@users.noreply.github.com>
2023-02-24 14:58:29 +0100

    WiP

--- pages/Chat/Daphne100k.tsx
+++ pages/Chat/Daphne100k.tsx
@@ -1,12 +1,37 @@
 import Link from 'next/link';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { daphData } from './remcostoeten-private-apiroutes/dData200k';
 import { auth, db, singInWithGoogle, logout } from '../../firebase';
 import data from './data.json';
 
-function Daphne() {
+function Daphne({handleKeyDown}) {
 	const [search, setSearch] = useState('');
 	const isLoggedIn = auth.currentUser;
+  const [name, setName] = useState("");
+  const [message, setMessage] = useState(data);
+
+  const filter = (e: { target: { value: any } }) => {
+    const keyword = e.target.value;
+
+	 const handleChange = (event) => {
+    setname(event.target.value);
+  };
+const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      handleAdd();
+    }
+  };
+    setName(keyword);
+  };
+
+let myClass = "class1";
+
+const setClass = () => {
+  data.forEach((item: { percent: number; }) => {
+    if(item.message.includes('remco')) myClass = 'class3'; console.log('a');
+  })
+}
+
 
 	return (
 		<>
@@ -14,14 +39,23 @@ function Daphne() {
 				<div className='chat__side-panel'>
 					<input
 						className='search'
-						placeholder='zoeken'
+					          onKeyDown={handleKeyDown}
+	placeholder='zoeken'
 						onChange={(e) => setSearch(e.target.value)}></input>
 				</div>
-				<div className='flex-wrap'>
-					{' '}
-					{data.chat.map((item, idx) => (
+				<div className='flex-wrap customers'>
+						{data.chat
+						.filter((item) => {
+							return search.toLowerCase() == ''
+								? item
+								: item.message.toLowerCase().includes(search);
+							<>
+								<div className='btn btn--primary'>btn</div>
+							</>;
+						})
+					.map((item, idx) => (
 						<div key={idx}>{item.message}</div>
-					))}
+						))}
 				</div>
 			</div>
 		</>
