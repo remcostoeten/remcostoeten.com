c68718aeed906d4c631660caa3b3951850264576
remcostoeten <remcostoeten@hotmail.com>
2023-02-26 17:43:52 +0100

    Stash

--- pages/Chat/chatDisplay.tsx
+++ pages/Chat/chatDisplay.tsx
@@ -1,8 +1,19 @@
 import React, { useState } from 'react';
 import { chatDisplayData } from '../api/chatDisplayData';
-function chatDisplay() {
-	const [search, setSearch] = useState('');
+import { auth, db, singInWithGoogle, logout } from '../../firebase';
 
+function chatDisplay() {
+	const isLoggedIn = auth.currentUser;
+	const [search, setSearch] = useState('');
+function userToClass(chatDisplayData){
+   var userClass = '';
+   if(chatDisplayData === 'You') {
+      userClass = 'left';
+   }
+   else 
+      userClass = 'right';
+   }
+}
 	return (
 		<div className='chat'>
 			<div className='chat__side-panel'>
@@ -11,6 +22,8 @@ function chatDisplay() {
 					placeholder='zoeken'
 					onChange={(e) => setSearch(e.target.value)}></input>
 			</div>
+			{isLoggedIn ? <h2>Logged in</h2> : <h3>Not loggedin</h3>}
+
 			<div className='chat__chat-panel chat-history'>
 				<div className='chat-history__inner'>
 					{chatDisplayData.chat
