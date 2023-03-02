e64c14880115d6590a90e80bd3495c408169958e
remcostoeten <remcostoeten@hotmail.com>
2023-03-02 19:22:29 +0100

    dd

--- types.ts
+++ types.ts
@@ -1,11 +1,3 @@
-export interface ChatMessage {
-	id: string;
-	message: string; // <-- add this property
-	type: 'sent' | 'received';
-	attachments: Attachment[];
-	sender: string;
-	timestamp: Date;
-}
 export interface Message extends ChatMessage {
 	message: string;
 	attachments: any;
@@ -16,5 +8,17 @@ export interface Message extends ChatMessage {
 export interface Attachment {
 	id: string;
 	type: string;
-	data: any;
+	data: string;
+	photo?: string;
+	format?: string;
+	device?: string; // Add device property
+}
+
+export interface ChatMessage {
+	id: string;
+	message: string;
+	type: 'sent' | 'received';
+	attachments?: Attachment[]; // updated attachments type
+	sender: string;
+	timestamp: Date;
 }
