bb3b07cd930a39a0caf3da2b8f08182518f87736
remcostoeten <57683378+remcostoeten@users.noreply.github.com>
2023-03-04 03:33:59 +0100

    Update search

--- pages/whatsapp-export/index.tsx
+++ pages/whatsapp-export/index.tsx
@@ -1,7 +1,7 @@
+import FeatureStory from '@/components/ChatSearch/Article';
 import React, { useEffect, useState } from 'react';
 import ChatSearch from '../../components/ChatSearch';
 import { ChatMessage, Attachment } from '../../types';
-
 const getChatHistory = (): ChatMessage[] => {
 	const chatHistoryRaw: any[] = require('../whatsapp-export/ChatHistory.json');
 	return chatHistoryRaw.map((msg: any): ChatMessage => {
@@ -36,47 +36,10 @@ const ChatHistory: React.FC = () => {
 	const [showFullText, setShowFullText] = useState(false);
 	const [chatHistory, setChatHistory] = useState<ChatMessage[]>([]);
 	const [showFullContent, setShowFullContent] = useState(false);
-
 	const githubLink =
 		'https://github.com/remcostoeten/remcostoeten.com/blob/develop/pages/whatsapp-export/index.tsx';
 
-	const featureStory = (
-		<>
-			Feature for me to practice hooks and other react/nextJS features.
-			<br></br> On WhatsApp there's the possibility to export your chats
-			which generates a plain .txt. I converted the txt to JSON objects
-			(some chats had almost up to a million lines). Once converted to
-			JSON I fetched the data, mapped over it and displayed it here.
-			<br></br> Besides that, I've added a search through the map
-			functionality. Initially, it was a filter functionality, but I much
-			prefer a jump to toggle like this. Also had to come up with a
-			solution for mobile since there's little space and flex-direction
-			row wouldn't work so I've made it that the search is toggleable in
-			an off-canvas menu. It used to be 35% width 100%height and absolute
-			but due to the toggleable anchors I had to make it sticky, not the
-			pretiest regarding relative styling but it'll do. You can find the
-			code for the search component{' '}
-			<a
-				target='_blank'
-				href='https://github.com/remcostoeten/remcostoeten.com/blob/develop/pages/whatsapp-export/index.tsx'>
-				{' '}
-				here
-			</a>{' '}
-			and{' '}
-			<a
-				target='_blank'
-				href='https://github.com/remcostoeten/remcostoeten.com/blob/develop/components/ChatSearch.tsx'>
-				{' '}
-				here
-			</a>
-			.
-		</>
-	);
-
-	// 	const featureStory = `Feature for me to practice hooks and other react/nextJS features. On WhatsApp there's the possibility to export your chats which generates a plain .txt. I converted the txt to JSON objects (some chats had almost up to a million lines). Once converted to JSON I fetched the data, mapped over it and displayed it here. Besides that, I've added a search through the map functionality. Initially, it was a filter functionality, but I much prefer a jump to toggle like this. Also had to come up with a solution for mobile since there's little space and flex-direction row wouldn't work so I've made it that the search is toggleable in an off-canvas menu. It used to be 35% width 100%height and absolute but due to the toggleable anchors I had to make it sticky, not the pretiest regarding relative styling but it'll do.
-
-	// You can find the code for the search component "<a href='#'>here</a>' and <a href='#'>here</a.
-	// And the page displaying the feature here: [HERE](https://github.com/remcostoeten/remcostoeten.com/blob/develop/pages/whatsapp-export/index.tsx)`;
+	<FeatureStory/>
 
 	const handleReadMoreClick = () => {
 		setShowFullText(true);
@@ -145,9 +108,7 @@ const ChatHistory: React.FC = () => {
 
 	return (
 		<>
-			<article>
-				<p>{featureStory}</p>
-			</article>
+		<FeatureStory/>
 			<ChatSearch
 				onSearch={handleSearch}
 				searchResults={searchResults}
