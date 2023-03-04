92f6a33a2bedef3d6fb74d15a17b3a53b483b41d
remcostoeten <remcostoeten@hotmail.com>
2023-03-04 15:23:45 +0100

    build warning

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,6 @@
 import '@/styles/styles.css';
 import Header from '@/components/header/Header';
 
-<link
-	href="'https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0&display=optional"
-	rel='stylesheet'
-/>;
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
