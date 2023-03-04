1bd04455e2946dc31544866e3bd7ea53def6c207
remcostoeten <remcostoeten@hotmail.com>
2023-03-04 15:15:17 +0100

    attempt to build master

--- pages/Chat/backups/window.jsx
+++
@@ -1,8 +0,0 @@
-import React, { useState, useEffect } from 'react';
-import { render } from 'react-dom';
-
-import MyGrid from './Components/MyGrid';
-import DataContext from './DataContext.js';
-export default function window() {
-	return <div>window</div>;
-}
