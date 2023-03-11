6cc8668120a88037735c97a1918071a97463cffe
remcostoeten <remcostoeten@hotmail.com>
2023-03-11 15:00:57 +0100

    update config to allow goolge images

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 const withTM = require('next-transpile-modules')(['json-loader']);
 
 module.exports = withTM({
-	webpack: (config) => {
+	images: {
+		domains: ['lh3.googleusercontent.com'],
+	},
+	pack: (config) => {
 		config.module.rules.push({
 			test: /\.json$/,
 			loader: 'json-loader',
