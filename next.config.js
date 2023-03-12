8d1f5ed77f97840910fc0916c0fd74bd87355713
remcostoeten <remcostoeten@hotmail.com>
2023-03-12 15:19:44 +0100

    revert config

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ const withTM = require('next-transpile-modules')(['json-loader']);
 module.exports = withTM({
 	images: {
 		domains: ['lh3.googleusercontent.com'],
-	    domains: ['lh3.googleusercontent.com'],
 	},
 	pack: (config) => {
 		config.module.rules.push({
