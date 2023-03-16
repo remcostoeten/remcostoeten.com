9379e077e52a76d81a86e51d9eedac19759c518e
remcostoeten <remcostoeten@hotmail.com>
2023-03-16 18:45:17 +0100

    Build

--- components/Todo/Todo.tsx
+++ components/Todo/Todo.tsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
+import Title from './components/Title';
+import AddTodo from './components/AddTodo';
+import Todo from './components/Todo';
 export default function Todo({
 	todo,
 	toggleComplete,
 	handleDelete,
 	handleEdit,
 }) {
-	const [newTitle, setNewTitle] = React.useState(todo.title);
+	const [newTitle, setNewTitle] = useState(todo?.title ?? '');
 
 	const handleChange = (e) => {
 		e.preventDefault();
@@ -20,31 +23,35 @@ export default function Todo({
 		}
 	};
 	return (
-		<div className='todo'>
-			<input
-				style={{ textDecoration: todo.completed && 'line-through' }}
-				type='text'
-				value={todo.title === '' ? newTitle : todo.title}
-				className='list'
-				onChange={handleChange}
-			/>
-			<div>
-				<button
-					className='button-complete'
-					onClick={() => toggleComplete(todo)}>
-					<CheckCircleIcon id='i' />
-				</button>
-				<button
-					className='button-edit'
-					onClick={() => handleEdit(todo, newTitle)}>
-					<EditIcon id='i' />
-				</button>
-				<button
-					className='button-delete'
-					onClick={() => handleDelete(todo.id)}>
-					<DeleteIcon id='i' />
-				</button>
+		<>
+			<div className='todo'>
+				<input
+					style={{
+						textDecoration: todo?.completed && 'line-through',
+					}}
+					type='text'
+					value={todo?.title === '' ? newTitle : todo?.title}
+					className='list'
+					onChange={handleChange}
+				/>
+				<div>
+					<button
+						className='button-complete'
+						onClick={() => toggleComplete(todo)}>
+						<CheckCircleIcon id='i' />
+					</button>
+					<button
+						className='button-edit'
+						onClick={() => handleEdit(todo, newTitle)}>
+						<EditIcon id='i' />
+					</button>
+					<button
+						className='button-delete'
+						onClick={() => handleDelete(todo.id)}>
+						<DeleteIcon id='i' />
+					</button>
+				</div>
 			</div>
-		</div>
+		</>
 	);
 }
--- pages/todo/index.tsx
+++ pages/todo/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import AddTodo from '@/components/todo/AddTodo';
 import Todo from '@/components/Todo/Todo';
 import {
@@ -8,8 +8,10 @@ import {
 	doc,
 	updateDoc,
 	deleteDoc,
+	where,
 } from 'firebase/firestore';
-import { db } from '@/base';
+import { auth, db } from '@/firebase';
+import Header from '@/components/header/Header';
 
 interface TodoItem {
 	title: string;
@@ -17,15 +19,24 @@ interface TodoItem {
 	completed: boolean;
 }
 
-export default function index() {
-	const [todos, setTodos] = React.useState<TodoItem[]>([]);
+export default function TodoList() {
+	const [todos, setTodos] = useState<TodoItem[]>([]);
+	const user = auth.currentUser;
 
 	useEffect(() => {
 		document.body.classList.add('todo-app');
-	}, []);
 
-	useEffect(() => {
-		const q = query(collection(db, 'todos'));
+		// Return early if the user is not logged in
+		if (!user) {
+			return;
+		}
+
+		// Create a reference to the todos collection for the user
+		const todosRef = collection(db, 'users', user.uid, 'todos');
+
+		// Create a query that only fetches todos for the current user
+		const q = query(todosRef, where('userId', '==', user.uid));
+
 		const unsub = onSnapshot(q, (querySnapshot) => {
 			const todosArray: TodoItem[] = [];
 			querySnapshot.forEach((doc) => {
@@ -34,24 +45,27 @@ export default function index() {
 			setTodos(todosArray);
 		});
 		return () => unsub();
-	}, []);
+	}, [user]);
 
 	const handleEdit = async (todo: TodoItem, title: string) => {
-		await updateDoc(doc(db, 'todos', todo.id), { title: title });
+		await updateDoc(doc(db, 'users', user?.uid, 'todos', todo.id), {
+			title: title,
+		});
 	};
 
 	const toggleComplete = async (todo: TodoItem) => {
-		await updateDoc(doc(db, 'todos', todo.id), {
+		await updateDoc(doc(db, 'users', user?.uid, 'todos', todo.id), {
 			completed: !todo.completed,
 		});
 	};
 
 	const handleDelete = async (id: string) => {
-		await deleteDoc(doc(db, 'todos', id));
+		await deleteDoc(doc(db, 'users', user?.uid, 'todos', id));
 	};
 
 	return (
 		<>
+			<Header />
 			<div className='todo'>
 				<div>
 					<AddTodo />
