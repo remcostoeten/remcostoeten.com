a02951b8fe7d84cc011a3c0bb3730b465ce20905
remcostoeten <remcostoeten@hotmail.com>
2023-03-23 13:17:02 +0100

    Add          warning to chat.

--- components/ui-elements/Warning.tsx
+++ components/ui-elements/Warning.tsx
@@ -1,51 +1,27 @@
 import * as React from 'react';
-import Stack from '@mui/material/Stack';
+import Alert from '@mui/material/Alert';
 import Button from '@mui/material/Button';
-import Snackbar from '@mui/material/Snackbar';
-import MuiAlert, { AlertProps } from '@mui/material/Alert';
+import Stack from '@mui/material/Stack';
 
-const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
-	props,
-	ref,
-) {
-	return <MuiAlert elevation={6} ref={ref} variant='filled' {...props} />;
-});
+export default function ActionAlerts() {
+	const [open, setOpen] = React.useState(true);
 
-export default function Warning() {
-	const [open, setOpen] = React.useState(false);
-
-	const handleClick = () => {
-		setOpen(true);
-	};
-
-	const handleClose = (
-		event?: React.SyntheticEvent | Event,
-		reason?: string,
-	) => {
-		if (reason === 'clickaway') {
-			return;
-		}
-
+	const handleClose = () => {
 		setOpen(false);
 	};
 
 	return (
-		<Stack spacing={2} sx={{ width: '100%' }}>
-			<Button variant='outlined' onClick={handleClick}>
-				Open success snackbar
-			</Button>
-			<Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
-				<Alert
-					onClose={handleClose}
-					severity='success'
-					sx={{ width: '100%' }}>
-					This is a success message!
-				</Alert>
-			</Snackbar>
-			<Alert severity='error'>This is an error message!</Alert>
-			<Alert severity='warning'>This is a warning message!</Alert>
-			<Alert severity='info'>This is an information message!</Alert>
-			<Alert severity='success'>This is a success message!</Alert>
+		<Stack>
+			{open && (
+				<div className='chat-warning'>
+					<Alert onClose={handleClose} severity='warning'>
+						This feature is abandoned. Hence the UI not being
+						perfect. The core functionallitys does work how
+						intended, just the styling which will get a rework,
+						there's a new version in the works!
+					</Alert>
+				</div>
+			)}
 		</Stack>
 	);
 }
