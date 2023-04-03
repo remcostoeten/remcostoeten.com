d007933122c4696997cfaf3cf55c850a763df600
remcostoeten <remcostoeten@hotmail.com>
2023-04-03 17:34:03 +0200

    add resume

--- next.config.js
+++ next.config.js
@@ -1,14 +1,30 @@
-const withTM = require('next-transpile-modules')(['json-loader']);
+const withTM = require('next-transpile-modules')(['react-pdf', 'json-loader']);
+
 module.exports = withTM({
 	images: {
 		domains: ['lh3.googleusercontent.com'],
 	},
-	webpack: (config) => {
+	webpack: (config, options) => {
 		config.module.rules.push({
 			test: /\.json$/,
 			loader: 'json-loader',
 			type: 'javascript/auto',
 		});
+
+		config.module.rules.push({
+			test: /\.(js|jsx)$/,
+			include: [/react-pdf/],
+			use: [
+				options.defaultLoaders.babel,
+				{
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env', '@babel/preset-react'],
+					},
+				},
+			],
+		});
+
 		return config;
 	},
 });
--- pages/y/index.tsx
+++ pages/y/index.tsx
@@ -1,5 +1,6 @@
 import { getChatHistory1 } from '@/utils/firebase';
 import withChat from '@/components/Chat/WithChat';
-
+import withAuth from '../withAuth';
+    
 const Chat1 = withChat(getChatHistory1);
-export default Chat1;
+export default withAuth(Chat1);
