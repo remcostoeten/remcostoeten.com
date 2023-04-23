72c23e01ce1a97cc341ce6a56a4b25d9a5d3b3d0
remcostoeten <57683378+remcostoeten@users.noreply.github.com>
2023-04-23 03:27:44 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -4,36 +4,14 @@ import '@/styles/styles.css';
 import Header from '../components/Header/Header';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import LottieLoader from '../components/ui-elements/LottieLoader';
 
 function App({ Component, pageProps }) {
-	const [isLoading, setIsLoading] = useState(false);
-	const router = useRouter();
-
-	useEffect(() => {
-		const handleRouteChangeStart = () => {
-			setIsLoading(true);
-		};
-		const handleRouteChangeComplete = () => {
-			setIsLoading(false);
-		};
-
-		// Listen for route changes to show and hide the loader
-		router.events.on('routeChangeStart', handleRouteChangeStart);
-		router.events.on('routeChangeComplete', handleRouteChangeComplete);
-
-		// Cleanup listeners
-		return () => {
-			router.events.off('routeChangeStart', handleRouteChangeStart);
-			router.events.off('routeChangeComplete', handleRouteChangeComplete);
-		};
-	}, []);
-
+	
 	return (
 		<>
 			<ToastContainer />
 			<Header />
-			{isLoading ? <LottieLoader /> : <Component {...pageProps} />}
+			<Component {...pageProps} />}
 		</>
 	);
 }
