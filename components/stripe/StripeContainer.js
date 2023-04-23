2212ed9ede31211275e5e5e59bbe906f95d16433
remcostoeten <remcostoeten@hotmail.com>
2023-04-23 18:28:19 +0200

    fix import

--- components/stripe/StripeContainer.js
+++ components/stripe/StripeContainer.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { loadStripe } from '@stripe/stripe-js';
 import { Elements } from '@stripe/react-stripe-js';
-
-import { CheckoutForm } from './CheckoutForm';
+import CheckoutForm from '../../pages/CheckoutForm';
 
 const PUBLIC_KEY = process.env.NEXT_PUBLIC_STRIPE_TEST_PUBLIC_KEY;
 const stripeTestPromise = loadStripe(PUBLIC_KEY);
