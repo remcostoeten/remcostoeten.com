54430cb9d54c9a0303fd2c0f19040528a8433554
remco stoeten <remcostoeten@hotmail.com>
2023-04-29 03:58:43 +0200

    bg

--- components/homepage/Hero.jsx
+++ components/homepage/Hero.jsx
@@ -4,13 +4,13 @@ import Image from 'next/image';
 
 export default function Hero() {
 	const [scrolled, setScrolled] = useState(false);
-	const [backgroundPosition, setBackgroundPosition] = useState('0 80px');
-	const [manPosition, setManPosition] = useState('0 0');
+	const [backgroundPosition, setBackgroundPosition] = useState('0 0');
+	const [manPosition, setManPosition] = useState(' 0');
 
 	useEffect(() => {
 		const handleScroll = () => {
 			const scrollY = window.scrollY;
-			setBackgroundPosition(`0 ${scrollY * 0.2}px`);
+			setBackgroundPosition(`0 -${scrollY * .4}px`);
 		};
 		window.addEventListener('scroll', handleScroll);
 		return () => window.removeEventListener('scroll', handleScroll);
@@ -18,7 +18,7 @@ export default function Hero() {
 	useEffect(() => {
 		const scrollMan = () => {
 			const scrollY = window.scrollY;
-			setManPosition(`0 -${scrollY * 2.2}px`);
+			setManPosition(` ${scrollY *.45}px`);
 		};
 
 		window.addEventListener('scroll', scrollMan);
@@ -32,22 +32,25 @@ export default function Hero() {
 					backgroundPosition,
 				}}>
 				<div className='container relative'>
-					{/* <div className={`hero__inner ${scrolled ? 'scrolled' : ''}`}> */}{' '}
+					<div className={`hero__inner ${scrolled ? 'scrolled' : ''}`}>
 					<Image
-						className='absolute right-0 top-0'
-						src='/man.png'
-						alt='hero'
-						style={{
-							backgroundPosition,
-						}}
-						width={400}
-						height={400}
-					/>{' '}
+  className='absolute right-0 top'
+  src='/man.png'
+  alt='hero'
+  style={{
+    transform: `translateY(${manPosition})`,
+	rotate: '${manPosition}',
+  }}
+  width={400}
+  height={400}
+/>
+
 					<h2 className=''>
 						remco
 						<br />
 						stoeten
 					</h2>{' '}
+					</div>
 				</div>
 			</div>
 		</main>
