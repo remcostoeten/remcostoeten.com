d5e779dd3922978b75e4a039c291d1fcb1835c39
remco stoeten <remcostoeten@hotmail.com>
2023-04-30 04:10:58 +0200

    b

--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,20 @@ import Section from '../components/homepage/Section';
 import WebGLCanvas from '../components/homepage/Orb';
 import Login from '../components/auth/old/Login';
 import React from 'react';
+import { Link } from 'react-scroll';
 
 export default function index() {
-	return (
-		<>
-			<Section />
-		
-		<WebGLCanvas/>
-			<div className='page-wrapper py-5 px-5'></div>
-		</>
-	);
+  return (
+    <>
+      <Section />
+      <Link
+        to='webgl-canvas'
+        smooth={true}
+        duration={500}
+      >
+        <WebGLCanvas id='webgl-canvas' />
+      </Link>
+      <div className='page-wrapper py-5 px-5'></div>
+    </>
+  );
 }
