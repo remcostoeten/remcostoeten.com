d31ed9aaec1d99b88f2d8870e230955b0b3a97ac
Remco Stoeten <remcostoeten@hotmail.com>
2023-05-15 14:47:59 +0200

    Update pills.

--- components/Header/OffcanvasMenuLinks.jsx
+++ components/Header/OffcanvasMenuLinks.jsx
@@ -99,7 +99,7 @@ export default function OffcanvasMenuLinks(props) {
 		<ul className='offcanvas-menu__items'>
 			{items.map((item) => (
 				<li
-					className='mb-8 md:mb-4   flex items-center'
+					className='mb-5 md:mb-4   flex items-center'
 					onClick={handleCloseMenu}
 					key={item.href}
 					value={item.wip}
