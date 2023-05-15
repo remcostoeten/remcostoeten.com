a01e5618fb3ea91bc0dd2900af5edfa5bc407272
Remco Stoeten <remcostoeten@hotmail.com>
2023-05-15 17:22:52 +0200

    add tailwind config

--- portfolio/tailwind.config.js
+++ portfolio/tailwind.config.js
@@ -8,10 +8,29 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
+        'offWhite': '#e4e7ec',
+        'offBlack': '#202328',
+        'blackAlternative': '#121212',
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      fontSize: {
+				xxs: ['10px'],
+				xs: ['12px'],
+				sm: ['14px'],
+				md: ['16px'],
+				lg: ['18px'],
+				xxl: ['40px'],
+				xxxl: ['50px'],
+				xxxxl: ['60px'],
+			},
+      padding: {
+        'xxs': '0.125rem', //8px'        
+        'small': '0.1875rem', //12px
+        'regular': '0.0625rem', //16px
+        'medium': '0.09375rem', //24px
+        'large': '0.125rem', //32px
     },
   },
   plugins: [],
