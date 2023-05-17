8ddd1b1be8846f9a401a63b56f03e55a2f8fc7c4
Remco Stoeten <remcostoeten@hotmail.com>
2023-05-17 21:58:40 +0200

    add menu animations

--- portfolio/components/header/Header.jsx
+++ portfolio/components/header/Header.jsx
@@ -1,14 +1,14 @@
 import React, { useEffect, useState } from "react";
-import Link from "next/link";
 import Logo from "./Logo";
 import Toggle from "./Toggle";
+import OffcanvasMenuLinks from "./OffcanvasMenuLinks";
 
 export default function Header() {
   const [menuOpen, setMenuOpen] = useState(false);
   const [sticky, setSticky] = useState(false);
 
   const handleCloseMenu = () => {
-    setMenuOpen(false);
+    setMenuOpen(!menuOpen);
   };
 
   useEffect(() => {
@@ -29,16 +29,35 @@ export default function Header() {
       } ${sticky ? "sticky" : "sticky"}`}
     >
       <div className="container justify-between items-center flex transition-width duration-500 ease-in-out pl-4 pt-8 pr-2">
-        <div
-          className="flex justify-between align-middle z-20"
-          onClick={handleCloseMenu}
-        >
+        <div className="flex z-20 justify-between" onClick={handleCloseMenu}>
           <Logo />
           <Toggle
             menuOpen={menuOpen}
             setMenuOpen={setMenuOpen}
             handleCloseMenu={handleCloseMenu}
           />
+        </div>
+        <div className="container">
+          {menuOpen && (
+            <div className="offcanvas-menu text-offWhite">
+              <div className="text-off-white">
+                <div>
+                  <div className="offcanvas-menu__menu--tagline mb-12 fadeIn ml-3 mt-10">
+                    <h2 className="animate__flipInX">Remco stoeten</h2>
+                    <p className="first">
+                      <span>Aspiring to be more</span>
+                    </p>
+                    <p className="last">
+                      than a <i>divjesschuiver</i>
+                    </p>
+                  </div>
+                  <div className="flex flex-col mb-8 mt-10 ml-2">
+                    <OffcanvasMenuLinks />
+                  </div>
+                </div>
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </div>
--- portfolio/components/header/Toggle.jsx
+++ portfolio/components/header/Toggle.jsx
@@ -1,27 +1,11 @@
-import React, { useState } from "react";
-import RetroButton from "@/components/ui-elements/buttons/RetroButton";
-import OffcanvasMenuLinks from "./OffcanvasMenuLinks";
+import React from "react";
 import styles from "./Toggle.module.scss";
 import MenuButton from "./offcanvas/MenuButton";
+import OffcanvasMenuLinks from "./OffcanvasMenuLinks";
 
-const Toggle = () => {
-  const [menuOpen, setMenuOpen] = useState(false);
-  const [hoveredClasses, setHoveredClasses] = useState([]); // Add hoveredClasses state
-  const parentClass = `offcanvas-menu ${hoveredClasses.join(" ")}`;
-
-  const handleCloseMenu = () => {
-    setMenuOpen(false);
-    document.body.classList.remove("offcanvas-open");
-  };
-
+const Toggle = ({ menuOpen, setMenuOpen, handleCloseMenu }) => {
   const handleToggle = () => {
     setMenuOpen(!menuOpen);
-    if (!menuOpen) {
-      document.body.classList.add("offcanvas-open");
-    } else {
-      document.body.classList.remove("offcanvas-open");
-      document.body.classList.remove("menu-delay");
-    }
   };
 
   return (
@@ -39,40 +23,6 @@ const Toggle = () => {
             <path d="M22,22 L2,22 C2,11 11,2 22,2 C33,2 42,11 42,22"></path>
           </symbol>
         </svg>
-        {menuOpen && (
-          <div className="offcanvas-menu absolute left-0 text-offWhite top-24">
-            <div className={styles.container + " text-off-white text-"}>
-              <div className="z-20 delayShow flex flex-col pl-8 md:pl-10 items-baseline">
-                <div className="offcanvas-menu__menu--tagline mb-12 fadeIn">
-                  <h2 className="animate__flipInX">Remco stoeten</h2>
-                  <p className="first">
-                    <span>Aspiring to be more</span>
-                  </p>
-                  <p className="last">
-                    than a <i>divjesschuiver</i>
-                  </p>
-                </div>
-                <div className="flex flex-col mb-8">
-                  <OffcanvasMenuLinks handleCloseMenu={handleCloseMenu} />
-                </div>
-                <div className="flex">
-                  <RetroButton
-                    text="Github"
-                    href="https://github.com/remcostoeten"
-                    handleCloseMenu={handleCloseMenu}
-                  />
-                  <span className="ml-4">
-                    <RetroButton
-                      text="Text or call"
-                      href="https://wa.me/31636590707"
-                      handleCloseMenu={handleCloseMenu}
-                    />
-                  </span>
-                </div>
-              </div>
-            </div>
-          </div>
-        )}
       </div>
     </>
   );
